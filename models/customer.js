10dc59f2e7f24fae9fd5f1115d21ba7913194a49
Nathan-Zenga <zengaman@uni.coventry.ac.uk>
2018-02-12 05:11:16 +0000

    exporting custom functions for passport config

--- models/customer.js
+++ models/customer.js
@@ -1,8 +1,8 @@
-var mongoose = require('mongoose'),
-	Schema = mongoose.Schema;
+var mongoose = require('mongoose');
+var bcrypt = require('bcrypt');
 
-// create schema and modal
-const CustomerSchema = new Schema({
+// create schema and model
+var UserSchema = mongoose.Schema({
 	firstName: { type: String },
 	lastName: { type: String },
 	email: { type: String },
@@ -11,6 +11,29 @@ const CustomerSchema = new Schema({
 	password: { type: String }
 });
 
-const Model = mongoose.model('Customer', CustomerSchema);
+var Customer = module.exports = mongoose.model('Customer', UserSchema);
 
-module.exports = Model;
+module.exports.createUser = function(newUser, callback){
+	bcrypt.genSalt(10, function(err, salt) {
+		bcrypt.hash(newUser.password, salt, function(err, hash) {
+			newUser.password = hash;
+			newUser.save(callback);
+		});
+	});
+}
+
+module.exports.getUserByEmail = function(email, callback){
+	var query = {email: email};
+	Customer.findOne(query, callback);
+}
+
+module.exports.getUserById = function(id, callback){
+	Customer.findById(id, callback);
+}
+
+module.exports.comparePassword = function(candidatePassword, hash, callback){
+	bcrypt.compare(candidatePassword, hash, function(err, isMatch) {
+		if(err) throw err;
+		callback(null, isMatch);
+	});
+}
