03cd4a57720753bf41e95da4fa4565514dbee702
Nathan-Zenga <zengaman@uni.coventry.ac.uk>
2018-02-12 05:19:02 +0000

    updates

--- app.js
+++ app.js
@@ -59,10 +59,6 @@ app.use( function(req, res, next) {
 	res.locals.success_msg = req.flash('success_msg');
 	res.locals.error_msg = req.flash('error_msg');
 	res.locals.error = req.flash('error');
-	next();
-});
-
-app.get('*', function(req, res, next){
 	res.locals.customer = req.customer || null;
 	next();
 });
