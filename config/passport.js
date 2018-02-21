04dbd514d6b7917a3b97675743cde54dfbd99277
Nathan-Zenga <zengaman@uni.coventry.ac.uk>
2018-02-21 21:00:38 +0000

    implementing custom queries from models file

--- config/passport.js
+++ config/passport.js
@@ -1,42 +1,35 @@
-const LocalStrategy = require('passport-local').Strategy,
-	  Customer = require('../models/customer'),
-	  config = require('../config/database'),
-	  bcrypt = require('bcrypt');
+var LocalStrategy = require('passport-local').Strategy,
+	Customer = require('../models/customer'),
+	config = require('../config/database'),
+	bcrypt = require('bcrypt');
 
 module.exports = function(passport) {
-	passport.use(new LocalStrategy(function(email, password, done){
-		
-		// match email
-		let query = {email: email};
-		Customer.findOne(query, function(err, user){
-			if (err) {
-				throw err
-			}
-			if (!user) {
-				return done(null, false, {message: 'User does not exist.'});
-			}
+	passport.use(new LocalStrategy(
+		function(username, password, done) {
+			Customer.getUserByUsername(username, function(err, user){
+				if(err) throw err;
+				if(!user){
+					return done(null, false, {message: 'Unknown user'});
+				}
 
-			// match password
-			bcrypt.compare(password, user.password, function(err, isMatch){
-				if (err) {
-					throw err
-				}
-				if (isMatch) {
-					return done(null, user);
-				} else {
-					return done(null, false, {message: 'Wrong password.'});
-				}
+				Customer.comparePassword(password, user.password, function(err, isMatch){
+					if(err) throw err;
+					if(isMatch){
+						return done(null, user);
+					} else {
+						return done(null, false, {message: 'Invalid password'});
+					}
+				});
 			});
-		});
-	}));
+		}));
 
 	passport.serializeUser(function(user, done) {
 		done(null, user.id);
 	});
 
 	passport.deserializeUser(function(id, done) {
-		Customer.findById(id, function(err, user) {
+		Customer.getUserById(id, function(err, user) {
 			done(err, user);
-		})
+		});
 	});
 }
