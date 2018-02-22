c08a254cd8f4910d700937db659ecd706166c736
Nathan-Zenga <zengaman@uni.coventry.ac.uk>
2018-02-22 03:44:04 +0000

    all updates for login process now functioning

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
 // import modules
-var express = require('express'),
-	session = require('express-session'),
+var express = require('express');
+	cookieParser = require('cookie-parser'),
 	bodyParser = require('body-parser'),
 	http = require('http'), // core module
 	path = require('path'), // core module
+	ejs = require('ejs'),
 	expressValidator = require('express-validator'),
 	flash = require('connect-flash'),
-	mongoose = require('mongoose'),
+	session = require('express-session'),
 	passport = require('passport'),
-	LocalStrategy = require('passport-local').Strategy,
-	database = require('./config/database'),
+	LocalStrategy = require('passport-local').Strategy;
+	mongoose = require('mongoose'),
+	config = require('./config/database'),
 	FACTORIAL = path.join(__dirname, 'build', 'factorial.min.js');
 
-mongoose.connect(database.database);
+mongoose.connect(config.db);
 let db = mongoose.connection;
 
 // Check connection
@@ -20,65 +22,59 @@ db.once('open', function() { console.log('Connected to db'); });
 // Check for DB errors
 db.on('error', function(err) { console.log(err); });
 
-// initialise express
+// Init App
 var app = express();
 
-// View engine
+// View Engine
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+// app.set('views', path.join(__dirname, 'views'));
+// app.engine('handlebars', exphbs({defaultLayout:'layout'}));
+// app.set('view engine', 'handlebars');
 
-// Initialize Passport (middleware)
-app.use(passport.initialize());
-app.use(passport.session());
+// BodyParser Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+// Set Static Folder
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Express Session
 app.use(session({
-	secret: 'secret',
-	saveUninitialized: true,
-	resave: true
+		secret: 'secret',
+		saveUninitialized: true,
+		resave: true
 }));
+
+// Passport init
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Express Validator
+app.use(expressValidator());
 
 // Connect Flash
 app.use(flash());
 
-// Global variables (in middleware)
-app.use( function(req, res, next) {
-	res.locals.db = null;
-	res.locals.ObjectId = null;
-	res.locals.errors = null;
+// Global Vars
+app.use(function (req, res, next) {
 	res.locals.success_msg = req.flash('success_msg');
 	res.locals.error_msg = req.flash('error_msg');
 	res.locals.error = req.flash('error');
+	res.locals.errors = null;
 	res.locals.user = req.user || null;
 	next();
 });
 
-// Body Parser middleware
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
 
 
-// Express Session Middleware
-app.use(session({
-	secret: 'test secret',
-	resave: true,
-	saveUninitialized: true
-}));
-
-// Express Validator middleware
-app.use(expressValidator());
-
-// set static path (joined with 'views')
-app.use(express.static(__dirname + '/public'));
-
-// preparing routes
 app.use('/', require('./routes/index'));
 app.use('/users', require('./routes/users'));
-app.use('/profile', require('./routes/profile'));
-app.use('/mail', require('./routes/mail'));
 
-// listen for requests on specified port
-var port = process.env.PORT || 5111;
-app.listen(port, function() {
-	console.log(`Good to go on port ${port}`);
+// Set Port
+app.set('port', (process.env.PORT || 5111));
+
+app.listen(app.get('port'), function(){
+	console.log('Server started on port '+app.get('port'));
 });
