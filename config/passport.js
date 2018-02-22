3698060fa1d20f5199bed0eb679fa96bbb79753f
Nathan-Zenga <zengaman@uni.coventry.ac.uk>
2018-02-22 12:20:46 +0000

    rewriting passport config

--- config/passport.js
+++ config/passport.js
@@ -1,34 +1,34 @@
 var LocalStrategy = require('passport-local').Strategy,
-	Customer = require('../models/customer'),
-	config = require('../config/database'),
-	bcrypt = require('bcrypt');
+	bcrypt = require('bcryptjs'),
+	User = require('../models/user'),
+	config = require('../config/config');
 
 module.exports = function(passport) {
 	passport.use(new LocalStrategy(
 		function(username, password, done) {
-			Customer.getUserByUsername(username, function(err, user){
-				if(err) throw err;
-				if(!user){
-					return done(null, false, {message: 'Unknown user'});
-				}
+			User.getUserByUsername(username, function(err, user){
+			if(err) throw err;
+			if(!user){
+				return done(null, false, {message: 'Unknown User'});
+			}
 
-				Customer.comparePassword(password, user.password, function(err, isMatch){
-					if(err) throw err;
-					if(isMatch){
-						return done(null, user);
-					} else {
-						return done(null, false, {message: 'Invalid password'});
-					}
-				});
+			User.comparePassword(password, user.password, function(err, isMatch){
+				if(err) throw err;
+				if(isMatch){
+					return done(null, user);
+				} else {
+					return done(null, false, {message: 'Invalid password'});
+				}
 			});
-		}));
+		});
+	}));
 
 	passport.serializeUser(function(user, done) {
 		done(null, user.id);
 	});
 
 	passport.deserializeUser(function(id, done) {
-		Customer.getUserById(id, function(err, user) {
+		User.getUserById(id, function(err, user) {
 			done(err, user);
 		});
 	});
