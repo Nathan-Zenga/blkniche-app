7aa5532f23c56e5d3f86e41bde38e1324b6ee65d
Nathan-Zenga <zengaman@uni.coventry.ac.uk>
2018-07-06 22:07:52 +0100

    edit post process

--- routes/blogposts.js
+++ routes/blogposts.js
@@ -2,27 +2,41 @@ var express = require('express'),
 	router = express.Router(),
 	Post = require('../models/post');
 
-router.post('/', function(req, res) {
-	var newPost = new Post({
-		userId: req.user._id.toString(),
-		title: req.body.title,
-		textbody: req.body.textbody
-	});
+router.post('/', (req, res) => {
+	if (req.body.title || req.body.textbody) {
+		var newPost = new Post({
+			userId: req.user._id.toString(),
+			title: req.body.title,
+			textbody: req.body.textbody
+		});
 
-	console.log(newPost);
-	console.log();
-
-	newPost.save((err, post) => {
-		if(err) throw err;
-		console.log(post);
+		newPost.save((err, post) => {
+			if(err) throw err;
+			res.redirect(req.get('referer'));
+		});
+	} else {
+		req.flash('error', 'No content added');
 		res.redirect(req.get('referer'));
-	});
+	}
 });
 
-router.delete('/delete/:id', function(req, res) {
+router.delete('/delete/:id', (req, res) => {
 	Post.remove({_id: req.params.id}, (err, result) => {
 		if (err) return err;
 		res.redirect(req.get('referer'));
+	})
+});
+
+router.post('/update/:id', (req, res) => {
+	Post.findById(req.params.id, (err, post) => {
+		if (err) return err;
+
+		post.title = req.body.title || post.title;
+		post.textbody = req.body.textbody || post.textbody;
+		post.save(err => {
+			if (err) return err;
+			res.redirect(req.get('referer'));
+		})
 	})
 });
 
