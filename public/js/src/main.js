f1e075e7d1468019f1d271a52b2f11de1406822e
Nathan-Zenga <zengaman@uni.coventry.ac.uk>
2019-10-29 00:56:36 +0000

    excluding use of randomColour function to make way for new colour scheme; modifying parameters for replace method

--- public/js/src/main.js
+++ public/js/src/main.js
@@ -15,14 +15,14 @@ $(function() {
 	}
 
 	// TEST (for each section bg): generate random colours
-	function randomColour() {
+	/*function randomColour() {
 		var arr = [];
 
 		for (var i = 0; i < 3; i++) arr.push(Math.round(Math.random() * 255));
 
 		arr = "rgb(" + arr.join(",") + ")";
 		return arr;
-	}
+	}*/
 
 	function smoothScrollTo(offsetTop) {
 		$("html, body").stop().animate({ scrollTop: offsetTop }, 700, "easeInOutExpo")
@@ -156,11 +156,11 @@ $(function() {
 	}, 4000);
 
 	// TEST: sets random colours as background color each page
-	$("section").each(function(){
+	/*$("section").each(function(){
 		$(this).css({
 			backgroundColor: randomColour()
 		});
-	});
+	});*/
 
 	$("#menu").click(function() {
 		$(this).toggleClass("is-active");
@@ -197,7 +197,7 @@ $(function() {
 			)
 			.end().find(".carousel-inner").append(
 				'<div class="item'+ (i < 1 ? ' active' : '') +'">' +
-					'<video class="img" loop controls playsinline><source src="'+ video.big.replace("_z.jpg", "_l.mp4") +'"/></video>' +
+					'<video class="img" loop controls playsinline><source src="'+ video.big.replace(/_b.jpg|_z.jpg/, "_l.jpg") +'"/></video>' +
 					'<div class="carousel-caption">' +
 						'<h3 class="carousel-caption-title">'+ video.title +'</h3>' +
 						'<p class="carousel-caption-artist">'+ description.artist +'</p>' +
